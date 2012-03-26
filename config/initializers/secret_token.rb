523bfc1539066ae958c265c0c218ab3940570d54
YogiZoli <zoltanp.gero@gmail.com>
2012-03-26 20:00:23 +0200

    Awesome Rails tutorial for refreshing:)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awesomepassion::Application.config.secret_token = '83eb6c68418f2d1e42aa8b6c09d0b200c4c9a1a036e7f9f7b4fbea42ea15efe86b38bd8d53719af53d5f2f405c3a9996f1355c938480a4c968cd869c037dd231'
