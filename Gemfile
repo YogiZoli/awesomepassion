967af99b1750717c395ea22687d3b6c7ba1b2fd9
YogiZoli <zoltanp.gero@gmail.com>
2012-03-26 22:10:54 +0200

    added content and title specs and created about page

--- Gemfile
+++ Gemfile
@@ -13,11 +13,11 @@ end
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.9.0'
-  gem 'cucumber-rails', '1.3.0'
 end
 
 group :test do
   gem 'capybara', '1.1.2'
+    gem 'cucumber-rails', '1.3.0'
 end
 
 # Gems used only for assets and not required
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "Home Page" do
+
+    it "should have the content 'Home'" do
+      visit '/static_pages/home'
+      page.should have_content('Home')
+    end
+    
+    it "should have right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Home")
+    end
+    
+    it "should have right h1 tag with Home" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => "Home")
+    end
+
+    
+  end # describe Home Page
+    
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+
+    it "should have right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Help")
+    end
+
+    it "should have right h1 tag with Help" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => "Help")
+    end
+
+
+  end # describe Help Page
+  
+  describe "About page" do
+  
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      page.should have_content('About')
+    end
+    
+    it "should have right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "About")
+    end
+    
+    it "should have right h1 tag with About" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => "About")
+    end
+
+  end # describe About PAge
+  
+  
+end #end of StaticPages
+
