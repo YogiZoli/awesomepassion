e49efb2abd4775a20f51983c9625ad02de1c7294
YogiZoli <zoltanp.gero@gmail.com>
2012-03-27 18:33:21 +0200

    replaced all CSS with SCSS, then created spec for contact, then created routes for it, then the controller method then the contacts views. Lastly changed all routes in spec for named routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,17 +7,17 @@ describe "StaticPages" do
   describe "Home Page" do
 
     it "should have the content 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Home')
     end
     
     it "should have right title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text => "#{base_title} | Home")
     end
     
     it "should have right h1 tag with Home" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => "Home")
     end
 
@@ -27,17 +27,17 @@ describe "StaticPages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
 
     it "should have right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', :text => "#{base_title} | Help")
     end
 
     it "should have right h1 tag with Help" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => "Help")
     end
 
@@ -47,22 +47,40 @@ describe "StaticPages" do
   describe "About page" do
   
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About')
     end
     
     it "should have right title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', :text => "#{base_title} | About")
     end
     
     it "should have right h1 tag with About" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => "About")
     end
 
   end # describe About PAge
-  
+
+    describe "Contact Page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      page.should have_content('Contact')
+    end
+    
+    it "should have right title" do
+      visit contact_path
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+    end
+    
+    it "should have right h1 tag with Home" do
+      visit contact_path
+      page.should have_selector('h1', :text => "Contact")
+    end
+    
+  end # describe Contect Page
   
 end #end of StaticPages
 
