4931ec2b1d6d67e07c0fc75641cb78dbbf77581e
YogiZoli <zoltanp.gero@gmail.com>
2012-03-27 13:04:07 +0200

    disabled test::unit from Spork and configured RSpec as default test engine

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,9 @@ module Awesomepassion
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.generators do |g|
+        g.test_framework :rspec #=> or whatever
+    end
   end
 end
