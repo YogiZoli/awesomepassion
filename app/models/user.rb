6b2d466dc551d5247c62da1f5865a48c6afd99a7
YogiZoli <zoltanp.gero@gmail.com>
2012-04-03 05:20:49 +0200

    fixed typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,6 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                 uniqueness: { case_sensitive: false }
-  validates :password, length { minumim: 6 }
+  validates :password, length: { minumim: 6 }
   validates :password_confirmation, presence: true
 end
