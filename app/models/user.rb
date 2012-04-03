16dcb162b68f361967d06368ff876d0152ed4c83
YogiZoli <zoltanp.gero@gmail.com>
2012-04-03 05:59:10 +0200

    Figured out MassAssignment error with tap, then changed user model now all spec passes

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,10 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password 
   
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, :length => { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                 uniqueness: { case_sensitive: false }
-  validates :password, length: { minumim: 6 }
+  validates :password, :length => { :in => 6..20 }
   validates :password_confirmation, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,8 +14,13 @@ require 'spec_helper'
 describe User do
 
   before do
-     @user = User.new(name:"Example", email:"example@gmail.com", 
-                                password: "foobar", password_confirmation: "foobar" )
+    @user = User.new(name:"Example", email:"example@gmail.com").tap do |u|
+        u.password = "foobar"
+        u.password_confirmation = "foobar"
+      end
+#    @user = User.new(name:"Example", email:"example@gmail.com")
+#    @user.password = "foobar"
+#    @user.password_confirmation = "foobar" 
   end
   
   subject { @user }
@@ -29,50 +34,50 @@ describe User do
   
   it { should be_valid }
   
-#  describe "When name is not exist" do
-#    before { @user.name = " " }
-#    it { should_not be_valid }
-#  end
+  describe "When name is not exist" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
   
-#  describe "When email is not exist" do
-#    before { @user.email = " "}
-#    it { should_not be_valid }
-#  end
+  describe "When email is not exist" do
+    before { @user.email = " "}
+    it { should_not be_valid }
+  end
   
-#  describe "When name is tooooo long" do
-#    before { @user.name="a"*51 }
-#    it { should_not be_valid }
-#  end
+  describe "When name is tooooo long" do
+    before { @user.name="a"*51 }
+    it { should_not be_valid }
+  end
   
-#  describe "when email format is invalid" do
-#    it "should be invalid" do
-#      addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
-#      addresses.each do |invalid_address|
-#        @user.email = invalid_address
-#        @user.should_not be_valid
-#      end      
-#    end
-#  end
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end      
+    end
+  end
 
-#  describe "when email format is valid" do
-#    it "should be valid" do
-#      addresses = %w[user@foo.com A_USER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-#      addresses.each do |valid_address|
-#        @user.email = valid_address
-#        @user.should be_valid
-#      end      
-#    end
-#  end
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.com A_USER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end      
+    end
+  end
   
-#  describe "When email address is already taken" do
-#    before do
-#      user_with_same_email = @user.dup
-#      user_with_same_email.email = @user.email.upcase
-#      user_with_same_email.save #fontos menteni!
-#    end
+  describe "When email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save #fontos menteni!
+    end
 
-#    it { should_not be_valid }
-#  end
+    it { should_not be_valid }
+  end
   
   describe "When password is not exist" do
     before { @user.password = @user.password_confirmation = " " } # double assignment!
