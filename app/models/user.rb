d9cbba0c75f1b0658c33fb4bbae0103a8e5ccc9a
YogiZoli <zoltanp.gero@gmail.com>
2012-04-03 05:17:14 +0200

    added Rails has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
-  has_secure_password
+  has_secure_password 
   
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
