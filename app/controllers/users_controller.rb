ce33683c7b766d3cf21e1b8b161b9e9280b9fd60
YogiZoli <zoltanp.gero@gmail.com>
2012-04-06 19:03:37 +0200

    if-else for signup failure, if it fails it redirects to new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-
   def new
     @user = User.new
+  end
+  
+  def create 
+    @user = User.new(params[:id])
+    if @user.save
+      #Handle success
+    else
+      render 'new'
+    end
   end
 end
