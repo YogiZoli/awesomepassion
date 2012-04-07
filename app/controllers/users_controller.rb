def3f5d288b07a4b67c79986d295776131f98005
YogiZoli <zoltanp.gero@gmail.com>
2012-04-08 00:33:08 +0200

    Added flash msg to the controller too

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(params[:user])
     if @user.save
+      flash[:success] = "Welcome to my Awesome App!"
       redirect_to @user
     else
       render 'new'
