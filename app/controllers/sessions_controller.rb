70f00c8b4a376f165dc46a5a44649eca48e8aadc
YogiZoli <zoltanp.gero@gmail.com>
2012-04-08 04:58:23 +0200

    checks if submitted info is vaild or not, if not rerenders the page, if user is exist AND has a valid pw then it passes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      # sign in the user
+    else
+      flash[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
