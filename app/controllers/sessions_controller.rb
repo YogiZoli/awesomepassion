fb76fc0c3fb3957fee361f65b63db5e5e34f2f6a
YogiZoli <zoltanp.gero@gmail.com>
2012-04-08 05:10:26 +0200

    Added SessionsHelper to AppController, which is accessible through full Rails app

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      # sign in the user
+      sign_in user
+      redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
